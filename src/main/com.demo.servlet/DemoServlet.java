49784090f0c84ef47f46af9d14ceb1637c42a886
gera-garza <gerardo.guillermo.gt@gmail.com>
2021-08-16 15:15:09 -0500

    baseline of servlets

---
+++ src/main/com.demo.servlet/DemoServlet.java
@@ -0,0 +1,9 @@
+@WebServlet("/DemoServlet")
+public class DemoServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello World!</h3>");
+    }
+}
